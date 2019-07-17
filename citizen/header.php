daeb93a531eafe69a803ac7213a5a5bcd9873495
Catherine IRUNGU <kathyjiku31@gmail.com>
2019-07-17 11:32:08 +0300

    search

--- citizen/header.php
+++ citizen/header.php
@@ -75,7 +75,7 @@
 			      </li>
 			    </ul>
 			    <form class="form-inline my-2 my-lg-0" action="search.php" method="POST">
-			      <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search" aria-label="Search">
+			      <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search projects" aria-label="Search">
 			      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit" name="search_btn">Search</button>
 			    </form>
 			  </div>
